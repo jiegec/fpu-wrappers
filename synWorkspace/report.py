ff07f4095b3618bbee929dc022c83438ad7449e1
Jiajie Chen <c@jia.je>
2021-09-05 08:26:30 +0800

    Add reports

---
+++ synWorkspace/report.py
@@ -0,0 +1,37 @@
+import os
+import glob
+import collections
+
+data = collections.defaultdict(lambda :{})
+
+for file_name in glob.glob('*/*_report_area.txt'):
+	parts = file_name.split('/')
+	key = parts[0]
+	with open(file_name) as f:
+		for line in f:
+			line = line.strip()
+			if line.startswith('Combinational area:'):
+				area = line.split(' ')[-1]
+				data[key]['comb_area'] = float(area)
+			if line.startswith('Total area:'):
+				area = line.split(' ')[-1]
+				data[key]['area'] = float(area)
+
+for file_name in glob.glob('*/*_report_timing_setup.txt'):
+	parts = file_name.split('/')
+	key = parts[0]
+	with open(file_name) as f:
+		for line in f:
+			line = line.strip()
+			if 'data arrival time' in line:
+				time = line.split(' ')[-1]
+				data[key]['max_comb_delay'] = float(time)
+				break
+
+keys = data.keys()
+for key in sorted(keys):
+	value = data[key]
+	print('{}:'.format(key))
+	print('  Area: {}'.format(value['area']))
+	print('  Comb Area: {}'.format(value['comb_area']))
+	print('  Max Freq: {:.0f} MHz'.format(1000.0 / value['max_comb_delay']))
