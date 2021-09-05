fc9cb7444bfacb8a5c88ac8952404f792e5cbbdd
Jiajie Chen <c@jia.je>
2021-09-05 15:04:10 +0800

    Add flopoco double fma

--- fpu-wrappers/src/main/resources/flopoco/gen.sh
+++ fpu-wrappers/src/main/resources/flopoco/gen.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 FLOPOCO=~/flopoco/build/flopoco
 
-$FLOPOCO IEEEFMA wE=8 wF=23 name=FMA_S IEEEFMA wE=5 wF=10 name=FMA_H
+$FLOPOCO IEEEFMA wE=11 wF=52 name=FMA_D \
+	IEEEFMA wE=8 wF=23 name=FMA_S \
+	IEEEFMA wE=5 wF=10 name=FMA_H
