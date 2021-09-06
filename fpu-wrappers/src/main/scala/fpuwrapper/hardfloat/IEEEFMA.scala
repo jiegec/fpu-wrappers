7fcfad487d27e86c81921118fd857263fbe68996
Jiajie Chen <c@jia.je>
2021-09-06 23:18:58 +0800

    Add ieee fma for hardfloat

---
+++ fpu-wrappers/src/main/scala/fpuwrapper/hardfloat/IEEEFMA.scala
@@ -0,0 +1,157 @@
+package fpuwrapper.hardfloat
+
+import chisel3._
+import chisel3.util._
+import chisel3.experimental._
+import fpuwrapper._
+
+class IEEEFMARequest(val floatType: FloatType, val lanes: Int) extends Bundle {
+  val op = FMAOp()
+  val operands = Vec(3, Vec(lanes, UInt(floatType.width.W)))
+}
+
+class IEEEFMAResponse(val floatType: FloatType, val lanes: Int) extends Bundle {
+  // result
+  val res = Vec(lanes, UInt(floatType.width.W))
+  // exception status
+  val exc = Vec(lanes, Bits(5.W))
+}
+
+class IEEEFMA(floatType: FloatType, lanes: Int, stages: Int) extends Module {
+  val io = IO(new Bundle {
+    val req = Flipped(Valid(new IEEEFMARequest(floatType, lanes)))
+    val resp = Valid(new IEEEFMAResponse(floatType, lanes))
+  })
+
+  val one = Wire(Vec(lanes, UInt(floatType.widthHardfloat().W)))
+  val zero = Wire(Vec(lanes, UInt(floatType.widthHardfloat().W)))
+  for (i <- 0 until lanes) {
+    one(i) := floatType.oneHardfloatChisel()
+    zero(i) := 0.U
+  }
+
+  // fma: neg * (op[0] * op[1]) + sign * op[2]
+  // neg: {0 => 1, 1 => -1}
+  // sub: {0 => 1, 1 => -1}
+  val op1 = Wire(Vec(lanes, UInt(floatType.widthHardfloat().W)))
+  val op2 = Wire(Vec(lanes, UInt(floatType.widthHardfloat().W)))
+  val op3 = Wire(Vec(lanes, UInt(floatType.widthHardfloat().W)))
+  for (i <- 0 until lanes) {
+    op1(i) := floatType.toHardfloat(io.req.bits.operands(0)(i))
+    op2(i) := floatType.toHardfloat(io.req.bits.operands(1)(i))
+    op3(i) := floatType.toHardfloat(io.req.bits.operands(2)(i))
+  }
+  val neg = WireInit(false.B)
+  val sign = WireInit(false.B)
+
+  // see the definition of FMAOp for more detail
+  switch(io.req.bits.op) {
+    is(FMAOp.FADD) {
+      op1 := one
+    }
+    is(FMAOp.FSUB) {
+      op1 := one
+      sign := true.B
+    }
+    is(FMAOp.FMUL) {
+      op3 := zero
+    }
+    is(FMAOp.FMADD) {
+      // do nothing
+    }
+    is(FMAOp.FMSUB) {
+      sign := true.B
+    }
+    is(FMAOp.FNMSUB) {
+      neg := true.B
+    }
+    is(FMAOp.FNMADD) {
+      neg := true.B
+      sign := true.B
+    }
+  }
+
+  // when stages > 3, add extra stages
+  val extraStages = (stages - 3) max 0
+  val inputStages = extraStages / 2
+  val outputStages = extraStages - inputStages
+
+  // replicate small units for higher throughput
+  val reqValid = io.req.valid
+  val results = for (i <- 0 until lanes) yield {
+    // MulAddRecFNPipe only support stages <= 3
+    val fma = Module(
+      new MulAddRecFNPipe(
+        stages min 3,
+        floatType.exp(),
+        floatType.sig()
+      )
+    )
+    fma.suggestName(s"fma_${floatType.kind()}_${i}")
+    fma.io.validin := Pipe(reqValid, reqValid, inputStages).bits
+    fma.io.a := Pipe(
+      reqValid,
+      op1(i),
+      inputStages
+    ).bits
+    fma.io.b := Pipe(
+      reqValid,
+      op2(i),
+      inputStages
+    ).bits
+    fma.io.c := Pipe(
+      reqValid,
+      op3(i),
+      inputStages
+    ).bits
+
+    fma.io.op := Pipe(
+      reqValid,
+      Cat(neg, sign),
+      inputStages
+    ).bits
+    // TODO
+    fma.io.roundingMode := 0.U
+    fma.io.detectTininess := 0.U
+
+    val res =
+      Pipe(true.B, floatType.fromHardfloat(fma.io.out), outputStages).bits
+    val exc = Pipe(true.B, fma.io.exceptionFlags, outputStages).bits
+    (res, exc)
+  }
+
+  // collect result
+  val res = results.map(_._1)
+  // exception flags
+  val exc = results.map(_._2)
+
+  val resValid = ShiftRegister(reqValid, stages)
+
+  io.resp.valid := resValid
+  io.resp.bits.res := res
+  io.resp.bits.exc := exc
+}
+
+object IEEEFMA extends EmitHardfloatModule {
+  emitHardfloat(
+    (floatType, lanes, stages) => new IEEEFMA(floatType, lanes, stages),
+    "Hardfloat_IEEEFMA"
+  )
+}
+
+object IEEEFMASynth extends EmitHardfloatModule {
+  for (floatType <- Seq(FloatH)) {
+    val floatName = floatType.kind().toString()
+    for (stages <- Seq(2)) {
+      emitHardfloat(
+        (floatType, lanes, stages) => new IEEEFMA(floatType, lanes, stages),
+        "Hardfloat_IEEEFMA",
+        allStages = Seq(stages),
+        floatTypes = Seq(floatType),
+        lanes = Seq(1)
+      )
+      val name = s"Hardfloat_IEEEFMA_${floatName}1l${stages}s"
+      Synthesis.build(Seq(s"${name}.v"), s"${name}_FMA", s"hardfloat_${name}")
+    }
+  }
+}
---
+++ fpu-wrappers/src/test/scala/fpuwrapper/hardfloat/IEEEFMATest.scala
@@ -0,0 +1,61 @@
+package fpuwrapper.hardfloat
+
+import chisel3._
+import chisel3.tester._
+import org.scalatest.FreeSpec
+import chiseltest.experimental.TestOptionBuilder._
+import chiseltest.internal.VerilatorBackendAnnotation
+import chiseltest.internal.WriteVcdAnnotation
+import chisel3.experimental.BundleLiterals._
+import chiseltest.legacy.backends.verilator.VerilatorFlags
+import fpuwrapper.FloatS
+
+class IEEEFMATest extends FreeSpec with ChiselScalatestTester {
+  for (stages <- 1 to 5) {
+    s"IEEEFMA of ${stages} stages should work" in {
+      test(new IEEEFMA(FloatS, 2, stages))
+        .withAnnotations(
+          Seq(
+            WriteVcdAnnotation
+          )
+        ) { dut =>
+          dut.clock.step(16)
+
+          def enqueueReq() {
+            dut.io.req.valid.poke(true.B)
+            dut.clock.step(1)
+            dut.io.req.valid.poke(false.B)
+          }
+
+          def expectResp()(x: IEEEFMA => Unit) {
+            val expectedCycles = stages - 1
+            var cycles = 0
+            while (dut.io.resp.valid.peek().litToBoolean == false) {
+              dut.clock.step(1)
+              cycles += 1
+            }
+            dut.io.resp.valid.expect(true.B)
+            x(dut)
+            dut.clock.step(1)
+            assert(
+              cycles == expectedCycles,
+              s"Response does not appear after expected cycles: ${cycles} != ${expectedCycles}"
+            )
+          }
+
+          dut.io.req.bits.operands(0)(0).poke("h3f800000".U) // 1
+          dut.io.req.bits.operands(1)(0).poke("h40000000".U) // 2
+          dut.io.req.bits.operands(2)(0).poke("h40400000".U) // 3
+          dut.io.req.bits.operands(0)(1).poke("h40800000".U) // 4
+          dut.io.req.bits.operands(1)(1).poke("h40a00000".U) // 5
+          dut.io.req.bits.operands(2)(1).poke("h40c00000".U) // 6
+          dut.io.req.bits.op.poke(FMAOp.FMADD)
+          enqueueReq()
+          expectResp() { dut =>
+            dut.io.resp.bits.res(0).expect("h40a00000".U) // 5
+            dut.io.resp.bits.res(1).expect("h41d00000".U) // 26
+          }
+        }
+    }
+  }
+}
