091b2154e288a1fe170c69ad1fcd5e1eefad06fb
Jiajie Chen <c@jia.je>
2021-09-13 14:19:36 +0800

    Print cells

--- fpu-wrappers/synWorkspace/report.py
+++ fpu-wrappers/synWorkspace/report.py
@@ -16,6 +16,9 @@ for file_name in glob.glob('*/*_report_area.txt'):
 			if line.startswith('Total area:'):
 				area = line.split(' ')[-1]
 				data[key]['area'] = float(area)
+			if line.startswith('Number of cells:'):
+				cells = line.split(' ')[-1]
+				data[key]['cells'] = int(cells)
 
 for file_name in glob.glob('*/*_report_timing_setup.txt'):
 	parts = file_name.split('/')
@@ -32,6 +35,7 @@ keys = data.keys()
 for key in sorted(keys):
 	value = data[key]
 	print('{}:'.format(key))
+	print('  Cells: {}'.format(value['cells']))
 	print('  Area: {}'.format(value['area']))
 	print('  Comb Area: {}'.format(value['comb_area']))
 	print('  Max Freq: {:.0f} MHz ({:.2f} ns)'.format(1000.0 / value['max_comb_delay'], value['max_comb_delay']))
