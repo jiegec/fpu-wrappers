509ec64c6c3ae43d2a68e80e09172c3d62de0b0e
Jiajie Chen <c@jia.je>
2023-07-06 09:37:51 +0800

    Bump nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,8 +1,9 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/9e5e77806a692277da477ca1879e24789350911.tar.gz") {}
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/8072d692ed0b32bf169e1391f12488b47406baaa.tar.gz") {}
 }:
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
-    circt # 1.34.0
+    circt # 1.40.0
+    mill
   ];
 }
